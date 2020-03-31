23f774bd8e8b6532fe76982eecf9bfeddaa7eb26
Mark Lucking <mark.lucking@gmail.com>
2020-03-31 20:35:00 +0200

    Cleared out magic numbers.

--- GeometrySpace/ContentView.swift
+++ GeometrySpace/ContentView.swift
@@ -18,52 +18,57 @@ struct Fonts {
     }
 }
 
-let backgrounds = [Color.red,Color.blue,Color.orange,Color.green]
+let minWidith = CGFloat(32)
+let minHeight = CGFloat(40)
+let fontSize = CGFloat(40)
 
+var backgrounds = [Color.blue,Color.orange,Color.green,Color(UIColor(red: 255/255, green: 105.0/255, blue: 180.0/255, alpha: 1.0)),Color.purple,Color.yellow,Color.red,Color(UIColor(red: 0/255, green: 255/255, blue: 0/255, alpha: 1.0)), Color(UIColor(red: 102/255, green: 178.0/255, blue: 178.0/255, alpha: 1.0))]
 struct ContentView: View {
   @State private var rect:[CGRect] = []
-  @State private var textText = [String](repeating: "", count: 16)
-  @State private var textColors = [Color](repeating: Color.clear, count: 16)
+  @State private var textText = [String](repeating: "", count: 100)
+  @State private var textColors = [Color](repeating: Color.clear, count: 100)
   @State private var textID:Int? = 0
-  @State private var textValue:[String] = [" 1 "," 2 "," 3 "," 4 "]
+  @State private var textValue:[String] = ["1","2","3","4","5","6","7","8","9"]
   
   
   var body: some View {
+    
     let dropDelegate = TheDropDelegate(textID: $textID, textText: $textText, rect: $rect, textColors: $textColors)
     return VStack {
     Spacer()
-      HStack(alignment: .center, spacing: 20) {
-      ForEach((0 ..< 4), id: \.self) { column in
+      HStack(alignment: .center, spacing: 8) {
+      ForEach((0 ..< textValue.count), id: \.self) { column in
         Text(self.textValue[column])
-          .font(Fonts.futuraCondensedMedium(size: 64))
+          .font(Fonts.futuraCondensedMedium(size: fontSize))
+          .frame(width: minWidith, height: minHeight, alignment: .center)
           .background(backgrounds[column])
-          .cornerRadius(32)
+          .cornerRadius(minHeight/2)
         .onDrag {
             return NSItemProvider(object: self.textValue[column] as NSItemProviderWriting) }
         }
       }
     
     Spacer()
-    VStack(alignment: .center, spacing: 5) {
-            ForEach((0 ..< 4).reversed(), id: \.self) { row in
-                HStack(alignment: .center, spacing: 5) {
-                    ForEach((0 ..< 4).reversed(), id: \.self) { column in
+    VStack(alignment: .center, spacing: 8) {
+            ForEach((0 ..< self.textValue.count).reversed(), id: \.self) { row in
+                HStack(alignment: .center, spacing: 8) {
+                    ForEach((0 ..< self.textValue.count).reversed(), id: \.self) { column in
                        return VStack {
-                        if self.textColors[fCalc(c: column, r: row)] == Color.clear {
-                           Text(self.textText[fCalc(c: column, r: row)])
-                          .font(Fonts.futuraCondensedMedium(size:48))
-                          .frame(width: 64, height: 64, alignment: .center)
+                        if self.textColors[fCalc(c: column, r: row, x: self.textValue.count)] == Color.clear {
+                           Text(self.textText[fCalc(c: column, r: row, x: self.textValue.count)])
+                          .font(Fonts.futuraCondensedMedium(size:fontSize))
+                          .frame(width: minWidith, height: minHeight, alignment: .center)
                           .background(InsideView(rect: self.$rect))
                           .onDrop(of: ["public.utf8-plain-text"], delegate: dropDelegate)
                         } else {
-                          Text(self.textText[fCalc(c: column, r: row)])
+                          Text(self.textText[fCalc(c: column, r: row, x: self.textValue.count)])
                           .onTapGesture {
-                            self.textText[fCalc(c: column, r: row)] = ""
-                            self.textColors[fCalc(c: column, r: row)] = Color.clear
+                            self.textText[fCalc(c: column, r: row, x: self.textValue.count)] = ""
+                            self.textColors[fCalc(c: column, r: row, x: self.textValue.count)] = Color.clear
                           }
-                          .font(Fonts.futuraCondensedMedium(size:48))
-                          .frame(width: 64, height: 64, alignment: .center)
-                          .background(self.textColors[fCalc(c: column, r: row)])
+                          .font(Fonts.futuraCondensedMedium(size:fontSize))
+                          .frame(width: minWidith, height: minHeight, alignment: .center)
+                          .background(self.textColors[fCalc(c: column, r: row, x: self.textValue.count)])
                           .onDrop(of: ["public.utf8-plain-text"], delegate: dropDelegate)
                           
                         }
@@ -89,8 +94,8 @@ struct ContentView: View {
   }
 }
 
-func fCalc(c:Int, r:Int) -> Int {
-  return (c + (r*4))
+func fCalc(c:Int, r:Int, x:Int) -> Int {
+  return (c + (r*x))
 }
 
 //struct TextView: View {
@@ -126,7 +131,7 @@ struct InsideView: View {
          GeometryReader { geometry in
           Rectangle()
             .fill(Color.yellow)
-            .frame(width: 64, height: 64, alignment: .center)
+            .frame(width: minWidith, height: minHeight, alignment: .center)
             .opacity(0.5)
             .onAppear {
               self.rect.append(geometry.frame(in: .global))
@@ -210,13 +215,8 @@ struct TheDropDelegate: DropDelegate {
                         if let urlData = urlData as? Data {
                            let text = String(decoding: urlData, as: UTF8.self)
                            self.textText[self.textID!] = text
-                           switch text {
-                            case " 1 ": self.textColors[self.textID!] = Color.red
-                            case " 2 ": self.textColors[self.textID!] = Color.blue
-                            case " 3 ": self.textColors[self.textID!] = Color.orange
-                            // 4 is the last option
-                            default: self.textColors[self.textID!] = Color.green
-                           }
+                           // we need to subtract 1 cause array starts at zero
+                           self.textColors[self.textID!] = backgrounds[Int(text)! - 1]
                         }
                     }
                 }
